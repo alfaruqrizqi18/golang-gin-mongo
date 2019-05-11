75e383783f09df54d72dc34ea6714d61efe0396d
Rizqi Al-Faruq <alfaruqrizqi18@gmail.com>
2019-05-11 11:22:02 +0700

    add remove data using first_name

--- controllers/student.go
+++ controllers/student.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var studentCollection *mgo.Collection
@@ -20,6 +22,35 @@ func init() {
 }
 
 func GetAllStudent(c *gin.Context) {
+	var student []structs.Student
+	first_name := c.DefaultQuery("first_name", "")
+
+	if strings.EqualFold(first_name, "") {
+
+		error := studentCollection.Find(nil).All(&student)
+		if error != nil {
+			log.Fatal(error)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "student data",
+			"data":    student,
+		})
+
+	} else {
+
+		var detailStudent = structs.Student{}
+		first_name := c.DefaultQuery("first_name", "")
+		error := studentCollection.Find(bson.M{"first_name": &first_name}).One(&detailStudent)
+		if error != nil {
+			log.Fatal(error)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "detail student",
+			"data":    detailStudent,
+		})
+	}
 
 }
 
@@ -28,17 +59,33 @@ func StoreStudent(c *gin.Context) {
 	first_name := c.PostForm("first_name")
 	last_name := c.PostForm("last_name")
 	created_at := c.PostForm("created_at")
-	defer session.Close()
 
 	storeStudent := structs.Student{id, first_name, last_name, created_at}
 	error := studentCollection.Insert(&storeStudent)
 
 	if error != nil {
-		log.Fatal(error)
 	}
 
 	c.JSON(http.StatusCreated, gin.H{
 		"status":  http.StatusCreated,
 		"message": "new student created successfully",
+	})
+}
+
+func DeleteStudent(c *gin.Context) {
+	var beforeDelete, afterDelete []structs.Student
+
+	first_name := c.Param("first_name")
+	studentCollection.Find(nil).All(&beforeDelete)
+	error := studentCollection.Remove(bson.M{"first_name": first_name})
+	studentCollection.Find(nil).All(&afterDelete)
+	if error != nil {
+		log.Fatal(error)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":       http.StatusOK,
+		"message":      "1 student removed successfully",
+		"beforeDelete": beforeDelete,
+		"afterDelete":  afterDelete,
 	})
 }
--- route/route.go
+++ route/route.go
@@ -8,6 +8,8 @@ func MainRoute() {
 	stringApi := "api"
 	v1 := router.Group(stringApi + "/v1")
 	{
+		v1.GET("/student", controller.GetAllStudent)
+		v1.DELETE("/student/delete/:first_name", controller.DeleteStudent)
 		v1.POST("/student", controller.StoreStudent)
 	}
 	router.Run()
